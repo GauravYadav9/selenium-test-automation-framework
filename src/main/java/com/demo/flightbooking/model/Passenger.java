ebe23098346fa2dfe513568b9980c9e17fb061cf
GauravYadav9 <emailingtogaurav@gmail.com>
2025-12-13 11:09:00 +0530

    Add Page Object Model: base page and all page objects - Abstract BasePage with centralized timeout resolution via resolveTimeout() - HomePage, FlightSelectionPage, PurchasePage, ConfirmationPage - Pages delegate all interactions to WebDriverUtils (composition pattern) - Passenger record with masked toString() for credit card security - Page validation via isPageDisplayed() on all pages

---
+++ src/main/java/com/demo/flightbooking/model/Passenger.java
@@ -0,0 +1,52 @@
+package com.demo.flightbooking.model;
+
+import com.demo.flightbooking.utils.MaskingUtil;
+
+/**
+ * A data model representing a passenger and their booking details.
+ * This is a Java Record, a modern way to create immutable data-carrier classes.
+ *
+ * SECURITY: The toString() method is explicitly overridden to mask sensitive data
+ * like credit card numbers to prevent accidental exposure in logs and reports.
+ */
+public record Passenger(
+    String origin,
+    String destination,
+    String firstName,
+    String lastName,
+    String address,
+    String city,
+    String state,
+    String zipCode,
+    String cardType,
+    String cardNumber,
+    String month,
+    String year,
+    String cardName,
+    int age,
+    String gender) {
+
+    /**
+     * Overrides toString() to mask sensitive data like credit card number.
+     */
+    @Override
+    public String toString() {
+        return "Passenger[" +
+               "origin='" + origin + '\'' +
+               ", destination='" + destination + '\'' +
+               ", firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               ", address='" + address + '\'' +
+               ", city='" + city + '\'' +
+               ", state='" + state + '\'' +
+               ", zipCode='" + zipCode + '\'' +
+               ", cardType='" + cardType + '\'' +
+               ", cardNumber='" + MaskingUtil.maskCardNumber(cardNumber) + '\'' +
+               ", month='" + month + '\'' +
+               ", year='" + year + '\'' +
+               ", cardName='" + cardName + '\'' +
+               ", age=" + age +
+               ", gender='" + gender + '\'' +
+               ']';
+    }
+}
